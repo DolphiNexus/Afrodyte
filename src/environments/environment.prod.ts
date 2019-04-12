3156fd407f587a54aea89182738aaa3a8a240e02
DolphiNexus <jairo73456@gmail.com>
2019-04-12 09:24:51 -0700

    Added Credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase : {
-   apiKey: "YOUR_CREDENTIALS_HERE",
-   authDomain: "YOUR_CREDENTIALS_HERE",
-   databaseURL: "YOUR_CREDENTIALS_HERE",
-   projectId: "YOUR_CREDENTIALS_HERE",
-   storageBucket: "YOUR_CREDENTIALS_HERE",
-   messagingSenderId: "YOUR_CREDENTIALS_HERE"
- }
+    apiKey: "AIzaSyCuaTaTNQGbesPjBBR68Aze7CqzClrp02A",
+    authDomain: "afrodyte-ef4e6.firebaseapp.com",
+    databaseURL: "https://afrodyte-ef4e6.firebaseio.com",
+    projectId: "afrodyte-ef4e6",
+    storageBucket: "afrodyte-ef4e6.appspot.com",
+    messagingSenderId: "820987149035"
+  }
 };
